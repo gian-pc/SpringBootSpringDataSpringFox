cd4558401e5dcb67701915882b0106f431c47970
gian-pc <gpaucarcortez@gmail.com>
2024-07-13 00:31:07 -0500

    Inicio del proyecto

---
+++ src/main/java/com/gianpc/restapis/RestapisApplication.java
@@ -0,0 +1,13 @@
+package com.gianpc.restapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gianpc/restapis/RestapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gianpc.restapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
