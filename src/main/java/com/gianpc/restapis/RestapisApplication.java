b7d7107e838ba50ed39478667f615ffd3ddeba94
gian-pc <gpaucarcortez@gmail.com>
2024-07-15 14:40:13 -0500

    Agregar explicación de la anotación @SpringBootApplication

--- src/main/java/com/gianpc/restapis/RestapisApplication.java
+++ src/main/java/com/gianpc/restapis/RestapisApplication.java
@@ -3,6 +3,10 @@ package com.gianpc.restapis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// ComponentScan: El busca controllers, services y repositories para agregarlos al classpath
+// EnableAutoConfiguration: Configura automaticamente la aplicación, por ejemplo encuentra el H2 en el classpath y lo configura
+// SpringBootConfiguration: Clase de configuración de Spring Boot
+// SpringBootApplication: Anotacion que combina las tres anotaciones anteriores
 @SpringBootApplication
 public class RestapisApplication {
 
