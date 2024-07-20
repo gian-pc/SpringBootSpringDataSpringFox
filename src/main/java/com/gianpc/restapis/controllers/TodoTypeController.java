7a53b0e6761eca267164c7a1bdc8caa2932b37ff
gian-pc <gpaucarcortez@gmail.com>
2024-07-19 21:03:47 -0500

    Agregar un endpoint de lectura

--- src/main/java/com/gianpc/restapis/controllers/TodoTypeController.java
+++ src/main/java/com/gianpc/restapis/controllers/TodoTypeController.java
@@ -1,5 +1,6 @@
 package com.gianpc.restapis.controllers;
 
+import com.gianpc.restapis.domains.TodoType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,17 @@ public class TodoTypeController{
     @GetMapping("/hello")
     public String helllo(){
         return "Hello World from TodoTypeController";
+    }
+
+    /**
+     * expose GetMapping en el /read endpoint
+     * @return TodoType
+     */
+    @GetMapping("/read")
+    public TodoType readTodoType(){
+        TodoType todoType = new TodoType();
+        todoType.setCode("1");
+        todoType.setDescription("Todo para personal stuff");
+        return todoType;
     }
 }
