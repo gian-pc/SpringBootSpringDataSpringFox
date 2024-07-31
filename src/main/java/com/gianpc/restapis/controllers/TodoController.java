10ffdeb2fdc541494a8208bfb3af6a46083c7f2c
gian-pc <gpaucarcortez@gmail.com>
2024-07-30 23:41:28 -0500

    Agregar CRUD de Todo
    Se desarrollaron los métodos del controlador y la lógica de negocio en el servicio

--- src/main/java/com/gianpc/restapis/controllers/TodoController.java
+++ src/main/java/com/gianpc/restapis/controllers/TodoController.java
@@ -5,6 +5,8 @@ import com.gianpc.restapis.domains.TodoType;
 import com.gianpc.restapis.services.TodoService;
 import com.gianpc.restapis.services.TodoTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,23 +23,29 @@ public class TodoController {
     }
 
     // Spring MVC implementa todas estas anotaciones para hacer el CRUD de un Tod0
-    @PostMapping("/create")
-    public Todo create(){
-        //..
+    @PostMapping
+    public Todo create(@RequestBody Todo todo){ // lo que viene en el body de la peticion se mapea a un objeto Todo
+        return todoService.create(todo);
     }
 
-    @GetMapping("/read")
-    public Todo read(){
-        //..
+    @GetMapping("/{id}")
+    public Todo read(@PathVariable("id") Long id){
+        return todoService.findById(id);
     }
 
-    @PutMapping("/update")
-    public Todo update(){
-        //..
+    @PutMapping
+    public Todo update(@RequestBody Todo todo){
+        return todoService.update(todo);
     }
 
-    @DeleteMapping("/delete")
-    public void delete(){
-        //..
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id){
+        try {
+            todoService.delete(id);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
     }
+
 }
--- src/main/java/com/gianpc/restapis/services/TodoService.java
+++ src/main/java/com/gianpc/restapis/services/TodoService.java
@@ -1,7 +1,43 @@
 package com.gianpc.restapis.services;
 
+import com.gianpc.restapis.domains.Todo;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service // proveernos una instancia SINGLETON de esta clase
 public class TodoService {
+
+    // Creamos un map para guardar los todos
+    private static Map<Long, Todo> todoCollection = new HashMap<>();
+
+    private static long idCount = 1L;
+
+    public Todo create(Todo todo) {
+        todo.setId(idCount);
+        todoCollection.put(idCount, todo);
+        idCount++;
+        return todo;
+    }
+
+    public Todo findById(Long id) {
+        return todoCollection.get(id);
+    }
+
+    public Todo update(Todo todo) {
+        todo.setLastUpdated(new Date());
+        if (todo.isDone()){
+            todo.setDateDone(new Date());
+        }
+        todoCollection.put(todo.getId(), todo);
+        return todo;
+    }
+
+    public void delete (Long id) throws Exception{
+        if(todoCollection.remove(id) == null){
+            throw new Exception("No existe ese Id");
+        }
+    }
 }
