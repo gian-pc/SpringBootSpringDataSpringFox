0e66b8e5e253d04ce8feb5edc9a1e391151a8cde
gianpc <gpaucarcortez@gmail.com>
2024-10-03 19:23:37 -0500

    feat: Ajustes en la serialización y deserialización de TodoType - Añadidas anotaciones de Jackson para control de formato y orden de propiedades JSON en TodoType. - Añadidas anotaciones de JAXB para control de formato y orden de propiedades XML en TodoType. - Uso de @JsonFormat para formatear fechas en JSON. - Uso de @XmlTransient para ignorar campos específicos en XML.

--- src/main/java/com/gianpc/restapis/domains/TodoType.java
+++ src/main/java/com/gianpc/restapis/domains/TodoType.java
@@ -1,15 +1,28 @@
 package com.gianpc.restapis.domains;
 
-import jakarta.xml.bind.annotation.XmlRootElement;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.xml.bind.annotation.*;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
-@XmlRootElement
+@JsonPropertyOrder(alphabetic = true) // muestra los datos en orden alfabético
+//@JsonPropertyOrder({"description", "code"}) // ordenamiento personalizado
+//@JsonInclude(JsonInclude.Include.NON_NULL) // or NON_EMPTY, ALWAYS, CUSTOM
+@XmlRootElement // cambiamos el root element
+@XmlAccessorType(XmlAccessType.FIELD) // aplicar tecnica de serialización y deserialización
+@XmlType(propOrder = {"code","dateCreated","lastUpdated"}) // orden de los elementos en el xml
 public class TodoType  {
     private String code;
+    //@JsonIgnore // ignora el campo description en Json
+    @XmlTransient // ignora el campo description en Xml
     private String description;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private Date dateCreated;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private Date lastUpdated;
 }
