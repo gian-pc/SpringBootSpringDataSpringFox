8b917bd827238414505fb814a6aa7f0f00e9da9d
gianpc <gpaucarcortez@gmail.com>
2025-01-03 17:17:08 -0500

    Agregar Query methods a TodoRepository

--- src/main/java/com/gianpc/restapis/repositories/TodoRepository.java
+++ src/main/java/com/gianpc/restapis/repositories/TodoRepository.java
@@ -1,7 +1,10 @@
 package com.gianpc.restapis.repositories;
 
 import java.util.Date;
+import java.util.List;
+
 import com.gianpc.restapis.domains.Todo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -33,6 +36,18 @@ public interface TodoRepository extends CrudRepository<Todo, Long>, PagingAndSor
 
     // Eliminar por título y done
     long deleteByTitleAndDone(String title, boolean done);
+
+    // ---- QUERY METHODS ---- son para algo más elaborado que los finders methods
+
+    // Buscar por título y done
+    @Query("SELECT t FROM Todo t WHERE t.done = true") // Esto es un query de objetos no de tablas
+    List<Todo> readAllDone();
+
+    // Buscar por fecha de creación mayor o igual a la fecha dada y fecha de vencimiento igual a la fecha dada
+    @Query("SELECT t FROM Todo t WHERE t.dateCreated >= ?1 AND t.dueDate = ?2")
+    List<Todo> fetchTodos(Date dateCreated, Date dueDate);
+
+
 
 }
 
